33030e8ac2abcf3ab92ff7cff9c050482e4a3c7d
최원서 <cws98tiger@naver.com>
2025-12-26 20:20:57 +0900

    Feat: 다형성과 설계 문제 풀이

---
+++ 1. 기본편/01. 김영한_자바 입문/src/poly/ex/sender/SendMain.java
@@ -0,0 +1,14 @@
+package poly.ex.sender;
+
+//메일을 발송합니다: 환영합니다!
+//SMS를 발송합니다: 환영합니다!
+//페이스북에 발송합니다: 환영합니다!
+
+public class SendMain {
+    public static void main(String[] args) {
+        Sender[] senders = {new EmailSender(), new SmsSender(), new FaceBookSender()};
+        for (Sender sender : senders) {
+            sender.sendMessage("환영합니다!");
+        }
+    }
+}
